f98c4a35ef775e93cee6c9a1798ae2d62839f21f
MartinMesko <alegremato@gmail.com>
2022-11-26 22:36:39 +0100

    NewUser class refactored.

--- src/main/java/multiThreading/NewUser.java
+++ src/main/java/multiThreading/NewUser.java
@@ -10,11 +10,9 @@ public class NewUser {
         this.userName = userName;
         this.userGuid = userGuid;
     }
-
     public NewUser() {
 
     }
-
     @Override
     public String toString() {
         return userId +
@@ -22,30 +20,18 @@ public class NewUser {
                 "" + userName + '\'' +
                 "'" + userGuid;
     }
-
     public int getUserId() {
         return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getUserGuid() {
         return userGuid;
     }
 
-    public void setUserGuid(String userGuid) {
-        this.userGuid = userGuid;
-    }
 
 
 }
